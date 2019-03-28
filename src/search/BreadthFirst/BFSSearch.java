d40411d4969581c65bca6a6be90775acbeffb4f2
Viterkim <viktorkim2005@hotmail.com>
2019-03-28 16:51:32 +0100

    ya

---
+++ src/search/BreadthFirst/BFSSearch.java
@@ -0,0 +1,14 @@
+package search.BreadthFirst;
+
+import graphs.Vertex;
+import search.ISearch;
+
+import java.util.ArrayList;
+
+public class BFSSearch<T> implements ISearch<T> {
+
+    @Override
+    public ArrayList<Vertex<T>> search(Vertex<T> start, Vertex<T> end) {
+        return null;
+    }
+}
---
+++ src/search/BreadthFirst/Main.java
@@ -0,0 +1,7 @@
+package search.BreadthFirst;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sue jugurt");
+    }
+}
